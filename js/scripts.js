ddc34a8e93541e9b113dd9d0494667824114e12a
swarrier16 <swarrier16@gmail.com>
2016-04-23 05:21:52 -0400

    Added files via upload

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+
+  $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+        
+        
