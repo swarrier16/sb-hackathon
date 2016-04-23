d3523fb1ba3856fbd3712ef44c920cd0b720fa01
swarrier16 <swarrier16@gmail.com>
2016-04-23 05:25:02 -0400

    Delete scripts.js

--- hackathon/js/scripts.js
+++
@@ -1,9 +0,0 @@
- $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-
-  $(document).ready(function(){
-      $('.parallax').parallax();
-    });
-        
-        
